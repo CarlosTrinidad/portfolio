f40ac85e4bf80dc5cfe148d5748b0e28cc0e0fe7
frontend <frontend@sistemexico.com>
2021-03-11 21:27:23 -0600

    :star: Date sorter added

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,2 +1,6 @@
+import moment from "moment";
+
 export const stringSorter = (a: string, b: string) => a.localeCompare(b);
 export const numberSorter = (a: number, b: number) => a - b;
+export const dateSorter = (a: string, b: string) =>
+  moment(a, "YYYY-MM-DD").diff(moment(b, "YYYY-MM-DD"));
